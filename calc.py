015fd05eea9d9182be27c1e8519449757dbad124
Hao Phan <rinnphan01@gmail.com>
2025-11-15 18:19:22 -0800

    Lets modify and make a new one

--- calc.py
+++
@@ -1,6 +0,0 @@
-def add(a, b):
-    return a + b
-# testing
-
-def sub(a, b):
-    return a - b
--- tests/test_calc.py
+++
@@ -1,8 +0,0 @@
-from app.calc import add, sub  # import from the app package
-
-def test_add():
-    assert add(2, 3) == 5
-    # Testing github
-
-def test_sub():
-    assert sub(10, 4) == 6
